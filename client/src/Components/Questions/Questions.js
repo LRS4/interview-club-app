271ba87881b135de84a760acb2fe0506ee4d8157
LRS4 <lrspencer@hotmail.co.uk>
2019-12-06 14:49:17 +0000

    Add data chips to questions view

--- client/src/Components/Questions/Questions.js
+++ client/src/Components/Questions/Questions.js
@@ -6,6 +6,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Chip from '@material-ui/core/Chip';
 
 // https://blog.logrocket.com/crud-with-node-graphql-react/#
 
@@ -15,6 +16,7 @@ const GET_QUESTIONS = gql`
       id
       text
       votes
+      date
       difficulty
       sectors
       job_titles
@@ -39,11 +41,16 @@ function GetAllQuestions() {
   
   if (error) return <p>Error :(</p>;
 
-  return data.questions.map(({ id, text, votes, difficulty, sectors, job_titles }) => ( 
+  return data.questions.map(({ id, text, votes, date, difficulty, sectors, job_titles }) => ( 
     
     <List style={{ marginTop: 10 }} component="nav" aria-label="secondary mailbox folders">
-      <ListItem button key={id}>
-        <ListItemText primary={text} secondary={ votes + " others were asked this" } style={{ textAlign: "center" }} />
+      <ListItem alignItems="flex-start" button key={id}>
+        <ListItemText primary={text} secondary={
+          <span>
+            <Chip size="small" label={ votes + " others were asked this!" } />  
+            <Chip size="small" label={ "Added " + new Date(parseInt(date)).toDateString().replace("GMT+0000 (Greenwich Mean Time)", "") } />      
+          </span>
+        } style={{ textAlign: "center" }} />
       </ListItem>
       <Divider />
     </List>
