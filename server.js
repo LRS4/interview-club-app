983168553ae6fd4e4815961012ed77b8c2eb8224
LRS4 <lrspencer@hotmail.co.uk>
2020-04-12 16:03:44 +0100

    Amended server file to exclude local env vars in dev

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
+// Load local environment variables if not production
+if (process.env.NODE_ENV !== 'production') { 
+    require('dotenv').config() 
+}
 
 const app = express();
 const port = process.env.port || 5000;
