08fbd7e14a2c3bf52c6ee6a596ce84d58dbd2502
LRS4 <lrspencer@hotmail.co.uk>
2020-04-13 19:05:53 +0100

    Added view answer scaffold and conditional render

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/navbar.component.jsx';
 import QuestionsList from './components/Questions/questions-list.component.jsx';
 import EditQuestion from './components/Questions/edit-question.component.jsx';
 import CreateQuestion from './components/Questions/create-question.component.jsx';
+import AnswersList from './components/Answers/answers-list.component.jsx';
 import CreateUser from './components/Users/create-user.component.jsx';
 import PageNotFound from './components/Other/page-not-found.component.jsx';
 // import logo from './logo.svg';
@@ -18,6 +19,7 @@ function App() {
         <Route path='/' exact component={QuestionsList} />
         <Route path='/edit/:id' exact component={EditQuestion} />
         <Route path='/create' exact component={CreateQuestion} />
+        <Route path='/answers/:qid' exact component={AnswersList} />
         <Route path='/user' exact component={CreateUser} />
         <Route component={PageNotFound}></Route>
       </Switch>
