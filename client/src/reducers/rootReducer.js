8e419e0612f3c3966abb7f76fef8dc8243e4b41e
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-06-06 12:59:27 +0100

    Added auth and error reducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
 import questionsReducer from './questionsReducer';
+import authReducer from './authReducer';
+import errorReducer from './errorReducer';
+
 
 const rootReducer = combineReducers({
-    questions: questionsReducer
+    questions: questionsReducer,
+    error: errorReducer,
+    auth: authReducer
 });
 
 export default rootReducer;
