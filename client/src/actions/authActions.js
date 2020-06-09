c5172bc2193bb9b073e895080b8f1f3a2018deff
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-06-09 18:50:36 +0100

    Added login action

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -56,6 +56,32 @@ export const register = ({ username, email, password }) => dispatch => {
         })
 }
 
+// Login User
+export const login = ({ username, password }) => dispatch => {
+    // Headers
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    // Request body
+    const body = JSON.stringify({ username, password });
+
+    // Send request
+    axios.post('/auth', body, config)
+        .then(res => dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        }))
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status, 'LOGIN_FAIL'));
+            dispatch({
+                type: LOGIN_FAIL,
+            });
+        })
+}
+
 // Logout User
 export const logout = () => {
     return {
